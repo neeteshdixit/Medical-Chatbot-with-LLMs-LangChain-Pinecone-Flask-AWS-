7a72e437d6429b9b62f66ff031c02469a003c362
dixit <dixitneetesh857@gmail.com>
2025-09-15 02:41:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Neetesh Kumar",
+    author_email="dixitneetesh857@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
